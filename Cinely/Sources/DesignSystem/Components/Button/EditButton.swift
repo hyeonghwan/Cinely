3a012e480adc59fef55e9a8fdc0a3e98e0991034
pHH <gudghks56@gmail.com>
2025-07-31 23:08:54 +0900

    feat/design_system: EditButton 추가

--- Cinely/Sources/DesignSystem/Components/Button/EditButton.swift
+++ Cinely/Sources/DesignSystem/Components/Button/EditButton.swift
@@ -5,4 +5,22 @@
 //  Created by hwan on 7/31/25.
 //
 
-import Foundation
+import UIKit
+import Design
+
+final class EditButton: BaseButton {
+    
+    convenience init(title: String) {
+        self.init(frame: .zero)
+        defer { self.setTitle(title, for: .normal) }
+    }
+    
+    override func addAttributes() {
+        self.backgroundColor = .clear
+        self.setTitleColor(.white, for: .normal)
+        self.layer.cornerRadius = 21
+        self.layer.borderWidth = 1
+        self.layer.borderColor = Color.white.cgColor
+    }
+}
+
