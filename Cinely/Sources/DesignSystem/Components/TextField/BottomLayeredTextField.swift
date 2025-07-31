82b2ec493d15b578399ebfe0c658d469780d129a
pHH <gudghks56@gmail.com>
2025-07-31 22:15:11 +0900

    feat/design_system: Bottom Layered TextField 추가

--- Cinely/Sources/DesignSystem/Components/TextField/BottomLayeredTextField.swift
+++ Cinely/Sources/DesignSystem/Components/TextField/BottomLayeredTextField.swift
@@ -5,4 +5,49 @@
 //  Created by hwan on 7/31/25.
 //
 
-import Foundation
+import UIKit
+
+final class BottomLayerTextField: UITextField {
+    
+    var btBorderHeight: CGFloat = 1
+    var btBorderColor: CGColor = UIColor.white.cgColor
+    
+    private var bottomLayer: CALayer! = nil
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.textAlignment = .center
+        if #available(iOS 17.0 ,*) {
+            observeLayout()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if bottomLayer == nil {
+            bottomLayer = CAShapeLayer()
+            bottomLayer.frame = CGRect(
+                x: 0.0,
+                y: self.bounds.height - btBorderHeight,
+                width: self.bounds.width,
+                height: btBorderHeight
+            )
+            bottomLayer.backgroundColor = btBorderColor
+            self.layer.addSublayer(bottomLayer)
+        }
+    }
+    
+    @available(iOS 17.0, *)
+    private func observeLayout() {
+        let sizeTraits: [UITrait] = [UITraitVerticalSizeClass.self, UITraitHorizontalSizeClass.self]
+        registerForTraitChanges(sizeTraits) { (self: Self, previousTraitCollection: UITraitCollection) in
+            self.bottomLayer.removeFromSuperlayer()
+            self.bottomLayer = nil
+            self.setNeedsLayout()
+        }
+    }
+}
