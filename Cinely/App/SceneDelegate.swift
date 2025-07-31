f5a04a768a79344cab5a4f2a0f61b34915fee075
pHH <gudghks56@gmail.com>
2025-07-31 20:53:55 +0900

    feat/splash: Splash design 과 code base setting #1    - design: 2초간 thread sleep 기능구현

--- Cinely/App/SceneDelegate.swift
+++ Cinely/App/SceneDelegate.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import Design
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow()
+        window.windowScene = windowScene
+        window.rootViewController = OnboardingViewController()
+        Thread.sleep(forTimeInterval: 2)
+        self.window = window
+        self.window?.makeKeyAndVisible()
     }
 }
-
