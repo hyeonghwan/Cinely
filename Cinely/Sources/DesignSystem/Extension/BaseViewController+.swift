71435b2840dc4d3a9af4fbde3453cb1332873504
pHH <gudghks56@gmail.com>
2025-07-31 22:07:26 +0900

    refactor: BaseVC extension color 정의 함수 추가 - 앱 내부에서 일관된 Background style을 가져가기 위함

--- Cinely/Sources/DesignSystem/Extension/BaseViewController+.swift
+++ Cinely/Sources/DesignSystem/Extension/BaseViewController+.swift
@@ -5,4 +5,11 @@
 //  Created by hwan on 7/31/25.
 //
 
-import Foundation
+import UIKit
+import Design
+
+extension BaseViewController {
+    func setDefaultBackground() {
+        self.view.backgroundColor = .black
+    }
+}
