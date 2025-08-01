b777f3859eae34374a96a8ca44bdd013a8b493ef
pHH <gudghks56@gmail.com>
2025-08-01 17:14:29 +0900

    feat/main_screen: Profile Section 추가

--- Cinely/Sources/CinemaFeature/View/Components/ProfileView.swift
+++ Cinely/Sources/CinemaFeature/View/Components/ProfileView.swift
@@ -5,4 +5,94 @@
 //  Created by hwan on 7/31/25.
 //
 
-import Foundation
+import UIKit
+import Design
+
+final class ProfileContainerCell: BaseCollectionViewCell, CellIdentifialble {
+    private let profileHeaderView = ProfileView()
+    
+    override func addChild() {
+        self.contentView.addSubview(profileHeaderView)
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func addLayout() {
+        let height = profileHeaderView.heightAnchor.constraint(equalToConstant: 100)
+        height.priority = .defaultLow
+        height.isActive = true
+        NSLayoutConstraint.activate([
+            profileHeaderView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 12),
+            profileHeaderView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: 12),
+            profileHeaderView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant: -12)
+        ])
+    }
+    
+    private class ProfileView: BaseView {
+        private let nicknameLabel   = UILabel()
+        private let signUpDateLabel = UILabel()
+        private let angleImageView  = UIButton()
+        private let movieLikeBoxButton  = UIButton()
+        
+        override func addAttributes() {
+            self.backgroundColor = Color.white.withAlphaComponent(0.1)
+            self.layer.cornerRadius = 12
+            
+            nicknameLabel.textColor = Color.white
+            nicknameLabel.text = "달콤한 기모청바지"
+            nicknameLabel.font = Font.semiBold24
+            
+            signUpDateLabel.textColor = Color.lightGray
+            signUpDateLabel.text = "25.06.24 가입"
+            signUpDateLabel.font = Font.light12
+            
+            angleImageView.setImage(Icons.forword, for: .normal)
+            angleImageView.tintColor = Color.lightGray
+            
+            movieLikeBoxButton.backgroundColor = Color.green.withAlphaComponent(0.6)
+            movieLikeBoxButton.setTitleColor(Color.white, for: .normal)
+            movieLikeBoxButton.setAttributedTitle(
+                NSAttributedString(string: "0개의 무비박스 보관중",
+                                   attributes: [.font : Font.semiBold17,
+                                                .foregroundColor : Color.white]),
+                for: .normal
+            )
+            movieLikeBoxButton.layer.cornerRadius = 8
+        }
+        
+        override func addChild() {
+            self.addSubview(nicknameLabel)
+            self.addSubview(signUpDateLabel)
+            self.addSubview(angleImageView)
+            self.addSubview(movieLikeBoxButton)
+            
+            nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
+            signUpDateLabel.translatesAutoresizingMaskIntoConstraints = false
+            angleImageView.translatesAutoresizingMaskIntoConstraints = false
+            movieLikeBoxButton.translatesAutoresizingMaskIntoConstraints = false
+            
+            addLayout()
+        }
+        
+        private func addLayout() {
+            nicknameLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+            NSLayoutConstraint.activate([
+                nicknameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+                nicknameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+                
+                angleImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12),
+                angleImageView.centerYAnchor.constraint(equalTo: nicknameLabel.centerYAnchor),
+                angleImageView.widthAnchor.constraint(equalToConstant: 25),
+                angleImageView.heightAnchor.constraint(equalToConstant: 25),
+                
+                signUpDateLabel.trailingAnchor.constraint(equalTo: angleImageView.leadingAnchor),
+                signUpDateLabel.centerYAnchor.constraint(equalTo: nicknameLabel.centerYAnchor),
+                
+                movieLikeBoxButton.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 24),
+                movieLikeBoxButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+                movieLikeBoxButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+                movieLikeBoxButton.heightAnchor.constraint(equalToConstant: 44),
+                movieLikeBoxButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16)
+            ])
+        }
+    }
+}
