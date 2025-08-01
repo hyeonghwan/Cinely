4ddf1e9c2c70f532c74452ae3e3967e84cf35fff
pHH <gudghks56@gmail.com>
2025-08-01 17:15:14 +0900

    feat: main Screen component 구성

--- Cinely/Sources/CinemaFeature/View/Components/TodayMovieItemCell.swift
+++ Cinely/Sources/CinemaFeature/View/Components/TodayMovieItemCell.swift
@@ -5,4 +5,78 @@
 //  Created by hwan on 7/31/25.
 //
 
-import Foundation
+import UIKit
+import Design
+
+final class TodayMovieItemCell: BaseCollectionViewCell, CellIdentifialble {
+    private let movieImageView   = MovieImageView()
+    private let titleLabel       = UILabel()
+    private let descriptionLabel = UILabel()
+    private let heartButton      = UIButton()
+    
+    override func addAttributes() {
+        movieImageView.contentMode = .scaleAspectFill
+        movieImageView.layer.cornerRadius = 12
+        movieImageView.clipsToBounds = true
+        
+        // Test
+        titleLabel.text = "기생충"
+        movieImageView.backgroundColor = Color.white
+        
+        titleLabel.textColor = Color.white
+        titleLabel.font = Font.bold24
+        descriptionLabel.textColor = Color.white
+        descriptionLabel.font = Font.light14
+        descriptionLabel.numberOfLines = 3
+        
+        heartButton.tintColor = Color.green
+        heartButton.setImage(Icons.heart, for: .normal)
+        heartButton.setImage(Icons.heartFill, for: .selected)
+    }
+    
+    override func addChild() {
+        self.contentView.addSubview(movieImageView)
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(descriptionLabel)
+        self.contentView.addSubview(heartButton)
+        
+        movieImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        heartButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func addLayout() {
+        titleLabel.setContentCompressionResistancePriority(.required, for: .vertical)
+        titleLabel.setContentHuggingPriority(.required, for: .vertical)
+        descriptionLabel.setContentCompressionResistancePriority(.required, for: .vertical)
+        descriptionLabel.setContentHuggingPriority(.required, for: .vertical)
+        
+        NSLayoutConstraint.activate([
+            movieImageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            movieImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            movieImageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            movieImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 2 - 80),
+            
+            titleLabel.topAnchor.constraint(equalTo: movieImageView.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: heartButton.leadingAnchor, constant: -8),
+            
+            heartButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            heartButton.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            heartButton.widthAnchor.constraint(equalToConstant: 30),
+            heartButton.heightAnchor.constraint(equalTo: heartButton.widthAnchor),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            descriptionLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+        ])
+    }
+    
+    func set(with: TodayMovieModel) {
+        self.movieImageView.image = UIImage.splash
+        self.titleLabel.text = with.title
+        self.descriptionLabel.text = with.description
+    }
+}
--- Cinely/Sources/CinemaFeature/View/ViewControllers/CinemaMainViewController.swift
+++ Cinely/Sources/CinemaFeature/View/ViewControllers/CinemaMainViewController.swift
@@ -5,4 +5,51 @@
 //  Created by hwan on 7/31/25.
 //
 
-import Foundation
+import UIKit
+import Design
+
+final class CinemaMainViewController: BaseViewController {
+    
+    private let dataSources            = CinemaDataSource()
+    private lazy var collectionView    = CinemaCollectionView(dataSource: self.dataSources)
+    
+    override func addAttributes() {
+        setDefaultBackground()
+        setNavigationTint()
+        setNavigationBackButton()
+        navigationSetting()
+    }
+    
+    private func navigationSetting() {
+        self.navigationItem.title = "Cinely"
+        self.navigationItem.rightBarButtonItem
+        = UIBarButtonItem(image: Icons.magnifyingglass?.withTintColor(Color.green.withAlphaComponent(0.6)),
+                          style: .plain,
+                          target: self,
+                          action: #selector(moveToSearchDetail(_:)))
+    }
+    
+    override func addChild() {
+        self.view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func addLayout() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    override func binding() {
+        
+    }
+    
+    @objc
+    private func moveToSearchDetail(_ sender: Any) {
+        let searchVC = CinemaMovieSearchVIewController()
+        self.navigationController?.pushViewController(searchVC, animated: true)
+    }
+}
