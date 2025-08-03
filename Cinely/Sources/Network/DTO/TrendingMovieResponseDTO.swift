80ab6ec2db68c373507f64dde8799be04d93da4d
pHH <gudghks56@gmail.com>
2025-08-03 23:53:37 +0900

    feat: TrendingMovieResponseDTO Extension to vm, to fallback

--- Cinely/Sources/Network/DTO/TrendingMovieResponseDTO.swift
+++ Cinely/Sources/Network/DTO/TrendingMovieResponseDTO.swift
@@ -42,3 +42,44 @@ struct TrendingMovieResponseDTO: Decodable {
         case voteCount = "vote_count"
     }
 }
+
+extension TrendingMovieResponseDTO {
+    private func mapToIfEmptyFirst(first: String?, second: String?) -> String {
+        (first ?? "").isEmpty ? (second ?? "") : (first ?? "")
+    }
+    
+    func fallBack(other: TrendingMovieResponseDTO) -> TrendingMovieResponseDTO {
+        let overview = mapToIfEmptyFirst(first: self.overview, second: other.overview)
+        return TrendingMovieResponseDTO(
+            id: id,
+            adult: adult,
+            backdropPath: backdropPath,
+            title: self.title,
+            originalTitle: self.originalTitle,
+            overview: overview,
+            posterPath: self.posterPath,
+            mediaType: self.mediaType,
+            originalLanguage: self.originalLanguage,
+            genreIds: self.genreIds,
+            popularity: self.popularity,
+            releaseDate: self.releaseDate,
+            video: self.video,
+            voteAverage: self.voteAverage,
+            voteCount: self.voteCount
+        )
+    }
+    
+    func toVM(genres: [Int: Genre], configuration: ImageConfiguration) -> TodayMovieModel {
+        let overviewIfEmpty = "줄거리를 제공하지 않습니다"
+        return TodayMovieModel(
+            id: self.id,
+            postImage: configuration.getPosterPathSizeW500(filePath: self.posterPath ?? "none"),
+            title: self.title ?? "N/A",
+            description: (self.overview ?? overviewIfEmpty).isEmpty ? overviewIfEmpty : "\(self.overview ?? overviewIfEmpty)",
+            favorite: false,
+            genres: self.genreIds?.compactMap { genres[$0] } ?? ["N/A"],
+            voteAverage: self.voteAverage ?? 0,
+            releaseDate: self.releaseDate ?? ""
+        )
+    }
+}
