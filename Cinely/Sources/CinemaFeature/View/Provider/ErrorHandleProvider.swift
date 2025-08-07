86e2f88138080ad4e4dcb41c5d5153f0cd5711b6
pHH <gudghks56@gmail.com>
2025-08-07 18:22:40 +0900

    refactor: Replace tuple with ErrorMessage struct in error handling
    - 튜플 대신 ErrorMessage struct로 대체
    - uuid 기반으로 Equatable, Hashable 준수
    - 기본 에러 메시지 및 retry 클로저 포함
    - Alamofire 에러에 따른 메시지 반환 로직 유지

--- Cinely/Sources/CinemaFeature/View/Provider/ErrorHandleProvider.swift
+++ Cinely/Sources/CinemaFeature/View/Provider/ErrorHandleProvider.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 import RxSwift
-import HwanMacros
+
 import Alamofire
 
 protocol ErrorHandleProvider {
-    func convertToURLError(error: Error) -> (title: String, message: String)?
+    func convertToURLError(error: Error) -> ErrorMessage?
 }
 
 enum GlobalErrorType: Error {
@@ -28,14 +28,39 @@ enum GlobalErrorType: Error {
     }
 }
 
+struct ErrorMessage: Hashable, Equatable {
+    let uuid = UUID()
+    let title: String
+    let message: String
+    
+    static func ==(_ lhs: Self, _ rhs: Self) -> Bool {
+        lhs.uuid == rhs.uuid
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uuid)
+    }
+    
+    var retry: () -> Void = { }
+    
+    static var `default`: ErrorMessage {
+        ErrorMessage(title: "에러", message: "시스템 내부에서 발생했습니다. 관리자에게 문의 해주세요!")
+    }
+}
+
 final class DefaultErrorHandleProviderProvider: ErrorHandleProvider {
     static let shared: ErrorHandleProvider = DefaultErrorHandleProviderProvider()
-    
-    func convertToURLError(error: Error) -> (title: String, message: String)? {
+    func convertToURLError(error: Error) -> ErrorMessage? {
         if let AFError = error.asAFError {
             let title: String = "네트워크 에러"
             var message: String = "알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요."
+            
             switch AFError {
+            case let .responseValidationFailed(reason: reason):
+                switch  reason {
+                default:
+                    return nil
+                }
             case let .sessionTaskFailed(error: error):
                 if let urlError = error as? URLError {
                     switch urlError.code {
@@ -57,13 +82,13 @@ final class DefaultErrorHandleProviderProvider: ErrorHandleProvider {
                     default:
                         message = "일시적인 네트워크 오류가 발생했습니다.\n 잠시 후 다시 시도해주세요."
                     }
-                    return (title, message)
+                    return ErrorMessage(title: title, message: message)
                 }
             default:
-                return (title, message)
+                return ErrorMessage(title: title, message: message)
             }
         }
-        return ("시스템 에러", GlobalErrorType.unknownError(error).message)
+        return ErrorMessage(title: "시스템 에러",message: GlobalErrorType.unknownError(error).message)
     }
 }
 
