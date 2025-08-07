bba5da0a22bcedfb678acbc32eb2f983a586b753
pHH <gudghks56@gmail.com>
2025-08-07 20:05:45 +0900

    refactor: AppDelegate에 RxSwift import 추가

--- Cinely/App/AppDelegate.swift
+++ Cinely/App/AppDelegate.swift
@@ -6,10 +6,10 @@
 //
 
 import UIKit
+import RxSwift
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     let networkManager: NetworkManager = DefaultNetworkManager.shared
     
     let storage: PersistentStorage = DefaultAppStorage()
@@ -24,18 +24,15 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     )
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        sleep(2)
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
-
-
 }
-
