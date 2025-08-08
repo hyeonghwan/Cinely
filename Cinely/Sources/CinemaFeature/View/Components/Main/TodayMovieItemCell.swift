05816184cdc8c746ba2e3c48e198911230718b9d
pHH <gudghks56@gmail.com>
2025-08-08 19:41:53 +0900

    fix(search): fix Layout Tag CollectionView Width in Search TableView Cell - CollectionView Cell의 tagLabel width 줄어드는 버그 Fix

--- Cinely/Sources/CinemaFeature/View/Components/Main/TodayMovieItemCell.swift
+++ Cinely/Sources/CinemaFeature/View/Components/Main/TodayMovieItemCell.swift
@@ -31,7 +31,7 @@ final class TodayMovieItemCell: BaseCollectionViewCell, CellIdentifialble {
         movieImageView.backgroundColor = Color.white
         
         titleLabel.textColor = Color.white
-        titleLabel.font = Font.bold24
+        titleLabel.font = Font.bold21
         descriptionLabel.textColor = Color.white
         descriptionLabel.font = Font.light14
         descriptionLabel.numberOfLines = 3
