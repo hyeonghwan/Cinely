ea1ede34eacb46bf98802a1c760fa3fd31410f3f
pHH <gudghks56@gmail.com>
2025-08-08 16:50:57 +0900

    feat: AppState Binder 정의  - User + Favorite + Search Result, Genre, Image Configuration 상태 관리  - 앱 전역에서 사용되는 상태 를 Input을 binder 로 보내도록 구성      -> Binder를 통해 앱 State를 업데이트 하고 View가 반응 할 수 있도록 구성

--- Cinely/Sources/Global/State/AppState.swift
+++ Cinely/Sources/Global/State/AppState.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import HwanKit
-import HwanMacros
+
 import RxSwift
 
 typealias Genre = String
@@ -17,16 +17,23 @@ protocol AppState {
     var genresState: Observable<[Int: Genre]> { get }
     var searchResultState: Observable<[RecentSearchModel]> { get }
     var configurationState: Observable<ImageConfiguration> { get }
-    func update(user: User)
+    var favoriteMoviesState: Observable<[TodayMovieModel]> { get }
     
     /// 앱 시작시 실행
     /// 1. genres id Mapping API Call
-    /// 2. Recent Search Text
-    /// 3. TMDB Configuration API Call
+    /// 2. TMDB Configuration API Call
     func appBootStrap()
+    
+    var signUpBinder: Binder<User> { get }
+    var signOutBinder: Binder<Void> { get }
+    var favoriteListBinder: Binder<(movie: TodayMovieModel, isFavorite: Bool)> { get }
+    var changeNickNameBinder: Binder<String> { get }
+    var addRecentSearchBinder: Binder<RecentSearchModel> { get }
+    var removeRecentSearchBinder: Binder<RecentSearchModel> { get }
+    var removeAllRecentSearchBinder: Binder<Void> { get }
 }
 
-@Logging
+
 final class DefaultAppState: AppState {
     
     struct Dependency {
@@ -37,6 +44,34 @@ final class DefaultAppState: AppState {
     init(dependency: Dependency) {
         self.appProvider = dependency.appProvider
         self.appStorage = dependency.appStorage
+        
+        self._userState = BehaviorSubject<User>(
+            value: User(
+                nickname: Storage.userName,
+                likeCount: Storage.userLikeCount,
+                signUpDate: Storage.userSignUpDate.ISOStringToDotString()
+            )
+        )
+        self._searchResultState = BehaviorSubject<[RecentSearchModel]>(
+            value: Storage.recentSearchModels
+        )
+        self._favoriteMoviesState = BehaviorSubject<[TodayMovieModel]>(
+            value: Storage.favoriteMovie
+        )
+        
+        let genresDictionary: [Int: Genre] = [
+            28: "액션", 12: "모험", 37: "서부",
+            16: "애니메이션", 35: "코미디", 53: "스릴러",
+            80: "범죄", 99: "다큐멘터리", 10752: "전쟁",
+            18: "드라마", 10751: "가족", 878: "SF",
+            14: "판타지", 36: "역사", 10770: "TV 영화",
+            27: "공포", 10402: "음악", 9648: "미스터리",
+            10749: "로맨스"
+        ]
+        
+        self._genresState = BehaviorSubject<[Int: Genre]>(
+            value: genresDictionary
+        )
     }
     
     private var disposeBag = DisposeBag()
@@ -47,45 +82,20 @@ final class DefaultAppState: AppState {
     
 
     // MARK: State
-    private let _userState = BehaviorSubject<User>(value: .default)
-    private let _genresState = BehaviorSubject<[Int: Genre]>(value: [:])
-    private let _searchResultState = BehaviorSubject<[RecentSearchModel]>(value: [])
+    private let _userState: BehaviorSubject<User>
+    private let _searchResultState: BehaviorSubject<[RecentSearchModel]>
+    private let _favoriteMoviesState: BehaviorSubject<[TodayMovieModel]>
+    
+    private let _genresState: BehaviorSubject<[Int: Genre]>
     private let _configurationState = BehaviorSubject<ImageConfiguration>(value: ImageConfigurationDTO.init().toConfig())
 
     // MARK: Output
     var userState: Observable<User> { _userState.asObservable() }
-    var genresState: Observable<[Int: Genre]> { _genresState.asObservable() }
     var searchResultState: Observable<[RecentSearchModel]> { _searchResultState.asObservable() }
+    var favoriteMoviesState: Observable<[TodayMovieModel]> { _favoriteMoviesState.asObservable() }
+    
+    var genresState: Observable<[Int: Genre]> { _genresState.asObservable() }
     var configurationState: Observable<ImageConfiguration> { _configurationState.asObservable() }
-    
-    // TODO: Need Storage Dependency
-    func update(user: User) {
-        _userState.onNext(user)
-    }
-    
-    func update(searchModel: RecentSearchModel) {
-        do {
-            var currentSearches = try _searchResultState.value()
-            currentSearches.removeAll { $0.word == searchModel.word }
-            currentSearches.insert(searchModel, at: 0)
-            _searchResultState.onNext(currentSearches)
-        } catch {
-            assert(false, "failed Get SearchResult from Subject")
-        }
-    }
-    
-    private func update(genres: [GenreDTO]) {
-        // TODO: Storage 저장 여부 결정
-        let cachingGenre = genres.reduce(into: [Int: Genre]()) { map, next in
-            if map[next.id] == nil {
-                map[next.id] = next.name
-            }
-        }
-        for (key, value) in cachingGenre {
-            logger.log(level: .info, "key: \(key), value: \(value)")
-        }
-        _genresState.onNext(cachingGenre)
-    }
     
     func appBootStrap() {
         appProvider.fetchGenres()
@@ -93,11 +103,8 @@ final class DefaultAppState: AppState {
             .subscribe(
                 with: self,
                 onNext: { app, genresDTO in
-                    app.logger.log(level: .info, "genresDTO: \(String(describing: genresDTO))")
                     app.update(genres: genresDTO)
                 }, onError: { app, error in
-                    // TODO: Error 처리, network 실패 처리 -> (어떻게 할까.....)
-                    app.logger.log(level: .info, "error: \(String(describing: error))")
                 }
             )
             .disposed(by: disposeBag)
@@ -109,13 +116,113 @@ final class DefaultAppState: AppState {
                  onNext: { app, config in
                      app._configurationState.onNext(config)
                  }, onError: { app, error in
-                     app.logger.log(level: .info, "error: \(String(describing: error))")
                  }
              )
              .disposed(by: disposeBag)
-        
-//        _searchResultState.onNext(
-//            appStorage.loadStorage()
-//        )
+    }
+    
+    private func update(genres: [GenreDTO]) {
+        let cachingGenre = genres.reduce(into: [Int: Genre]()) { map, next in
+            if map[next.id] == nil {
+                map[next.id] = next.name
+            }
+        }
+        _genresState.onNext(cachingGenre)
+    }
+}
+
+
+extension DefaultAppState: ReactiveCompatible {
+    var signUpBinder: Binder<User> {
+        Binder<User>(self) { appState, user in
+            Storage.userName = user.nickname
+            Storage.userSignUpDate = user.signUpDate
+            Storage.userLikeCount = 0
+            var user = user
+            user.signUpDate = user.signUpDate.ISOStringToDotString()
+            appState._userState.onNext(user)
+        }
+    }
+    
+    var signOutBinder: Binder<Void> {
+        Binder<Void>(self) { appState, user in
+            appState._userState.onNext(.default)
+            appState._searchResultState.onNext([])
+            appState._favoriteMoviesState.onNext([])
+            Storage.userName = ""
+            Storage.userSignUpDate = ""
+            Storage.userLikeCount = 0
+            Storage.favoriteMovie = []
+            Storage.recentSearchModels = []
+        }
+    }
+
+    var favoriteListBinder: Binder<(movie: TodayMovieModel, isFavorite: Bool)> {
+        Binder<(movie: TodayMovieModel, isFavorite: Bool)>(self) { appState, list in
+            var current = Set<TodayMovieModel>(Storage.favoriteMovie)
+            let (movie, isFavorite) = list
+            
+            if movie.favorite == isFavorite {
+                return
+            }
+            
+            let trueModel = movie.copy(favorite: true)
+            if isFavorite {
+                current.insert(trueModel)
+            } else {
+                current.remove(trueModel)
+            }
+            if let user = try? appState._userState.value() {
+                let updatedUser = User(nickname: user.nickname, likeCount: current.count, signUpDate: user.signUpDate)
+                appState._userState.onNext(updatedUser)
+                Storage.userLikeCount = current.count
+            }
+            let nextState = Array(current)
+            appState._favoriteMoviesState.onNext(nextState)
+            Storage.favoriteMovie = nextState
+        }
+    }
+    
+    var changeNickNameBinder: Binder<String> {
+        Binder<String>(self) { appState, chageName in
+            if let user = try? appState._userState.value() {
+                appState._userState.onNext(
+                    User(
+                        nickname: chageName,
+                        likeCount: user.likeCount,
+                        signUpDate: user.signUpDate
+                    )
+                )
+                Storage.userName = chageName
+            }
+        }
+    }
+    
+    var addRecentSearchBinder: Binder<RecentSearchModel> {
+        Binder<RecentSearchModel>(self) { appState, searchModel in
+            if var currentSearchState = try? appState._searchResultState.value() {
+                currentSearchState.removeAll(where: { $0.word == searchModel.word })
+                currentSearchState.insert(searchModel, at: 0)
+                Storage.recentSearchModels = currentSearchState
+                appState._searchResultState.onNext(currentSearchState)
+            }
+        }
+    }
+    
+    var removeRecentSearchBinder: Binder<RecentSearchModel> {
+        Binder<RecentSearchModel>(self) { appState, searchModel in
+            if var currentSearchState = try? appState._searchResultState.value() {
+                currentSearchState.removeAll(where: { $0.word == searchModel.word })
+                Storage.recentSearchModels = currentSearchState
+                appState._searchResultState.onNext(currentSearchState)
+            }
+        }
+    }
+    
+    var removeAllRecentSearchBinder: Binder<Void> {
+        Binder<Void>(self) { appState, _ in
+            Storage.recentSearchModels = []
+            appState._searchResultState.onNext([])
+        }
     }
 }
