bf0fee0abceed2157e33d4ca37121db77cad47dc
pHH <gudghks56@gmail.com>
2025-08-08 16:49:16 +0900

    feat: Provider Error Subject configure + catch

--- Cinely/Sources/CinemaFeature/View/Provider/MovieSearchProvider.swift
+++ Cinely/Sources/CinemaFeature/View/Provider/MovieSearchProvider.swift
@@ -7,18 +7,23 @@
 
 import Foundation
 import RxSwift
-import HwanMacros
+import RxRelay
 
-protocol MovieSearchProvider {
+protocol MovieSearchProvider: Provider {
     func search(page: Int, query: String) -> Observable<MovieSearchApiResource.ResponseType>
 }
 
-@Logging
+
 final class DefaultMovieSearchProvider: MovieSearchProvider {
     private let networkManager: NetworkManager
+    var errorMessageSubscription: Observable<ErrorMessage?> {
+        self._errorMessageSubscription.asObservable()
+    }
+    private(set) var _errorMessageSubscription: PublishRelay<ErrorMessage?>
     
     init(networkManager: NetworkManager) {
         self.networkManager = networkManager
+        _errorMessageSubscription = PublishRelay<ErrorMessage?>()
     }
     
     func search(page: Int, query: String) -> Observable<MovieSearchApiResource.ResponseType> {
@@ -32,7 +37,11 @@ final class DefaultMovieSearchProvider: MovieSearchProvider {
             ),
             decodeType: MovieSearchApiResource.ResponseType.self,
             decoder: nil
-        )
+        ).catch { [weak self] error in
+            let errorMessage = DefaultErrorHandleProviderProvider.shared.convertToURLError(error: error)
+            self?._errorMessageSubscription.accept(errorMessage)
+            return Observable<MovieSearchApiResource.ResponseType>.just(.init(page: 0, totalPages: 0, totalResults: 0))
+        }
     }
 }
 
--- Cinely/Sources/CinemaFeature/View/Provider/TrendingMovieProvider.swift
+++ Cinely/Sources/CinemaFeature/View/Provider/TrendingMovieProvider.swift
@@ -7,19 +7,25 @@
 
 import Foundation
 import RxSwift
-import HwanMacros
+import RxRelay
 
-protocol TrendingMovieProvider {
+protocol TrendingMovieProvider: Provider {
     func fetchTrendingMovies() -> Observable<TrendingApiResource.ResponseType>
 }
 
-@Logging
+
 final class DefaultTrendingMovieProvider: TrendingMovieProvider {
     
     private let networkManager: NetworkManager
     
+    var errorMessageSubscription: Observable<ErrorMessage?> {
+        self._errorMessageSubscription.asObservable()
+    }
+    private(set) var _errorMessageSubscription: PublishRelay<ErrorMessage?>
+    
     init(networkManager: NetworkManager) {
         self.networkManager = networkManager
+        _errorMessageSubscription = PublishRelay<ErrorMessage?>()
     }
     
     func fetchTrendingMovies() -> Observable<PagedResponseDTO<TrendingMovieResponseDTO>> {
@@ -28,17 +34,23 @@ final class DefaultTrendingMovieProvider: TrendingMovieProvider {
             decodeType: PagedResponseDTO<TrendingMovieResponseDTO>.self,
             decoder: nil
         )
+        
         let en_us = networkManager.GET(
             resource: TrendingApiResource(query: .init(language: "en-US")),
             decodeType: PagedResponseDTO<TrendingMovieResponseDTO>.self,
             decoder: nil
         )
+        
         return Observable.zip(korean, en_us)
             .map { value in
                 var pagedDTO = value.0
                 pagedDTO.results = zip(value.0.results ?? [], value.1.results ?? [])
                     .map { ko, en in ko.fallBack(other: en) }
                 return pagedDTO
+            }.catch { [weak self] error in
+                let errorMessage = DefaultErrorHandleProviderProvider.shared.convertToURLError(error: error)
+                self?._errorMessageSubscription.accept(errorMessage)
+                return Observable<PagedResponseDTO<TrendingMovieResponseDTO>>.just(.init(page: 0, results: [], totalPages: 0, totalResults: 0))
             }
     }
 }
